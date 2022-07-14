afa7237e7b3c00f875a6beaab4b8423acc840494
Junlong Liu <junlong.liu@shopee.com>
2022-07-14 11:59:16 +0800

    overhead about gengine

--- gengine_overhead_benchmark_test.go
+++ gengine_overhead_benchmark_test.go
@@ -3,12 +3,24 @@ package go_rule_eng_research
 import (
 	"testing"
 
-	"github.com/bilibili/gengine/builder"
-	"github.com/bilibili/gengine/context"
 	"github.com/bilibili/gengine/engine"
 )
 
 // 引入规则引擎造成的性能损耗
+
+/*
+✦2 ❯ go test -bench=. -benchmem -test.bench BenchmarkX -cpuprofile cpuprofile.out
+goos: darwin
+goarch: amd64
+pkg: ruleng
+cpu: Intel(R) Core(TM) i9-9880H CPU @ 2.30GHz
+BenchmarkXGo-16                 554308380                2.050 ns/op           0 B/op          0 allocs/op
+BenchmarkXGengine-16              346998              3194 ns/op             816 B/op         18 allocs/op
+BenchmarkXGengineInGoFnc-16       599295              1876 ns/op             560 B/op          9 allocs/op
+PASS
+ok      ruleng  5.343s
+
+*/
 
 type ReqStruct struct {
 	A string
@@ -25,13 +37,12 @@ func newReq() *ReqStruct {
 	return req
 }
 
-func BenchmarkGo(b *testing.B) {
+func BenchmarkXGo(b *testing.B) {
 	req := newReq()
-	b.ResetTimer()
+	b.ResetTimer() // 重置计时器，忽略前面的准备时间
 	for n := 0; n < b.N; n++ {
 		ValidateByGo(req)
 	}
-	b.StopTimer()
 }
 func ValidateByGo(req *ReqStruct) bool {
 	if req.A != "A" {
@@ -53,44 +64,87 @@ const rulereq = `
 rule "rule1"
 begin
       if req.A != "A"{return "A"}
+      return ""
+end
+
+rule "rule2"
+begin
       if req.B != "B"{return "B"}
+      return ""
+end
+
+rule "rule3"
+begin
       if req.C.A != "A"{return "CA"}
+      return ""
+end
+
+rule "rule4"
+begin
       if req.C.B != "B"{return "CB"}
       return ""
 end
 `
 
-var eng = engine.NewGengine()
+var pool = &engine.GenginePool{}
 
-func BenchmarkGengine(b *testing.B) {
+func BenchmarkXGengine(b *testing.B) {
 	req := newReq()
-
-	dataContext := context.NewDataContext()
-	dataContext.Add("req", req)
-
-	ruleBuilder := builder.NewRuleBuilder(dataContext)
-
-	err := ruleBuilder.BuildRuleFromString(rulereq)
+	var err error
+	pool, err = engine.NewGenginePool(10, 100, 2, rulereq, make(map[string]interface{}))
 	if err != nil {
 		b.Fatal(err)
 	}
-	b.StartTimer()
+	datamap := map[string]interface{}{"req": req}
+
+	b.ResetTimer() // 重置计时器，忽略前面的准备时间
 	for n := 0; n < b.N; n++ {
-		_ = ValidateByGengine(ruleBuilder)
+		_ = ValidateByGengine(datamap, b)
 	}
-	b.StopTimer()
 }
 
-func ValidateByGengine(ruleBuilder *builder.RuleBuilder) bool {
-	err := eng.Execute(ruleBuilder, false)
-	if err != nil {
-		return false
-	}
-	res, _ := eng.GetRulesResultMap()
+const rulereq2 = `
+rule "rule1"
+begin
+    ValidateByGo(req)
+    return ""
+end
+`
+
+func ValidateByGengine(datamap map[string]interface{}, b *testing.B) bool {
+	_, res := pool.Execute(datamap, true)
 	for _, v := range res {
 		if v != "" {
 			return false
 		}
 	}
 	return true
+}
+
+func ValidateByGengineInGoFnc(datamap map[string]interface{}, b *testing.B) bool {
+	_, res := pool.Execute(datamap, true)
+	for _, v := range res {
+		if v != false {
+			return false
+		}
+	}
+	return true
+}
+
+func BenchmarkXGengineInGoFnc(b *testing.B) {
+	req := newReq()
+	var err error
+	pool, err = engine.NewGenginePool(10, 100, 2, rulereq2, make(map[string]interface{}))
+	if err != nil {
+		b.Fatal(err)
+	}
+	datamap := map[string]interface{}{
+		"req":          req,
+		"ValidateByGo": ValidateByGo,
+	}
+
+	b.ResetTimer() // 重置计时器，忽略前面的准备时间
+	for n := 0; n < b.N; n++ {
+		_ = ValidateByGengineInGoFnc(datamap, b)
+	}
 }
